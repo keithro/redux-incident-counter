a547d5501e1f4de6e92ef65f7053bd161d1e1386
Keith Rodriguez <keithrodriguez@rightpoint.com>
2022-07-26 14:42:06 -0400

    Set up actions, reducer and store

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+// ACTION TYPES
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET = 'SET';
+
+// ACTION CREATORS ///////////////////
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
+export const set = (set) => ({ type: SET, payload: value });
